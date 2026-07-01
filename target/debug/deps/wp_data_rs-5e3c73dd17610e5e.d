93cc57307c9b27cd66c1b64874385850166f47dc
Brandon Hubbard <bhubbard@users.noreply.github.com>
2026-07-01 09:23:41 -0700

    Add .gitignore/.gitattributes, untrack target/

--- target/debug/deps/wp_data_rs-5e3c73dd17610e5e.d
+++
@@ -1,7 +0,0 @@
-/Users/bhubbard/PROJECTS/elementor-to-blocks/packages/wp-data-rs/target/debug/deps/wp_data_rs-5e3c73dd17610e5e.d: src/lib.rs
-
-/Users/bhubbard/PROJECTS/elementor-to-blocks/packages/wp-data-rs/target/debug/deps/libwp_data_rs-5e3c73dd17610e5e.rlib: src/lib.rs
-
-/Users/bhubbard/PROJECTS/elementor-to-blocks/packages/wp-data-rs/target/debug/deps/libwp_data_rs-5e3c73dd17610e5e.rmeta: src/lib.rs
-
-src/lib.rs:
--- target/package/wp-data-rs-0.1.1/README.md
+++
@@ -1,3 +0,0 @@
-# wp-data-rs
-
-Rust port of WordPress/Elementor package wp-data-rs.
